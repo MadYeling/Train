336deef0d58afccff237e1bdb3e85c0e2dc046bc
madyeling <ycc360zaseycc360@sina.com>
2018-06-20 07:41:03 +0800

    Task06

---
+++ src/task06/Task06Test.java
@@ -0,0 +1,7 @@
+package task06;
+
+public class Task06Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/task06/Users.java
@@ -0,0 +1,59 @@
+package task06;
+
+import java.util.Date;
+
+public class Users {
+    private String name, password, email;
+    private Date birthday;
+    private int phoneNumber;
+
+    public Users(String name, String password, String email, Date birthday, int phoneNumber) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.birthday = birthday;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+}
